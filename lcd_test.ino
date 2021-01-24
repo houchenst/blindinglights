dd7f1c2dd9b55a57ca9701a2284bd4122a54f34d
Lawrence H <26679627+lawrenceh1850@users.noreply.github.com>
2021-01-24 12:55:49 -0500

    Add files via upload

---
+++ lcd_test.ino
@@ -0,0 +1,93 @@
+/*
+  LiquidCrystal Library - Hello World
+
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This sketch prints "Hello World!" to the LCD
+ and shows the time.
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+ modified 7 Nov 2016
+ by Arturo Guadalupi
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
+
+*/
+
+// include the library code:
+#include <LiquidCrystal.h>
+#include <Servo.h> 
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+int micValue;
+int normValue = 512;
+
+// Declare the Servo pin 
+int servoPin = 8; 
+// Create a servo object 
+Servo Servo1; 
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+//  lcd.print("hello, world!");
+  Serial.begin(9600);  // sets the serial port to 9600
+  lcd.clear();
+
+//   Servo1.attach(servoPin); 
+//  Servo1.write(0);
+}
+
+void loop() {
+  micValue = analogRead(0);       // read analog input pin 0
+  int numCols = abs(micValue - normValue);
+  lcd.clear();
+//  lcd.print(micValue);
+  for (int row=0;row <2;row++) {
+    for (int col=0; col < numCols; col++) {
+      // set the cursor to (column, row)
+      lcd.setCursor(col, row);
+      lcd.write(255);
+    }
+  }
+
+  int deg = numCols * 5;
+  if (deg > 90) {
+    deg = 90;
+  }
+Serial.println(deg, DEC);  // prints the value read  
+//  Servo1.write(deg);
+
+  delay(80);
+}
